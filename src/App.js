ec354ce1aaa771744400090859e66895dae94643
J <ghosts.of.princes.in.towers@gmail.com>
2022-10-29 18:45:42 -0500

    Fetch quotes from API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [quote, setQuote] = useState({});
+
+  useEffect(() => {
+    fetch('https://api.adviceslip.com/advice')
+      .then((res) => res.json() )
+      .then((res) => {
+        let data = res.slip;
+        setQuote(data);
+      })
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{quote.advice}</h1>
     </div>
   );
 }
