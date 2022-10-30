e9b6b8ba36804169d5372d52ee0832e7d7f4d970
J <ghosts.of.princes.in.towers@gmail.com>
2022-10-29 20:19:33 -0500

    add assets and styling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import './App.css';
 
 function App() {
   const [quote, setQuote] = useState({});
 
   useEffect(() => {
+    getQuote();
+  }, [])
+
+  const getQuote = useCallback(async () => {
     fetch('https://api.adviceslip.com/advice')
       .then((res) => res.json() )
       .then((res) => {
         let data = res.slip;
         setQuote(data);
       })
-  }, []);
+  }, [quote]);
 
   return (
     <div className="App">
-      <h1>{quote.advice}</h1>
+      <div className="container">
+        <h3 class="advice-title">Advice #{quote.id}</h3>
+        <h1 class="quote-text">"{quote.advice}"</h1>
+        <div className="divider"></div>
+      </div>
+      <button className="dice-btn" onClick={getQuote}></button>
     </div>
   );
 }
